e04a223ecc073a0767fa767774bbc21771dd0514
Eclipse-Walker <phisanurat.w@outlook.com>
2026-07-09 22:20:56 +0700

    feat: port profile viewer logic to background and clean up manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,20 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'Meta Downloader',
+    description: 'View & download Instagram and TikTok profile pictures in full size.',
+    permissions: ['contextMenus', 'declarativeNetRequest', 'downloads', 'scripting', 'tabs'],
+    host_permissions: [
+      'https://*.instagram.com/*',
+      'https://www.instagram.com/*',
+      '*://www.tiktok.com/*',
+    ],
+    action: {
+      default_title: 'View/download Instagram & TikTok profile picture',
+    },
+    declarative_net_request: {
+      rule_resources: [{ id: 'ruleset_1', enabled: true, path: 'rules.json' }],
+    },
+  },
 });
